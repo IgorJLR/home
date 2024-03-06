06d159a58c94f12ae83dd92a6891371187490f0b
Apotecaedro <140354981+Apotecaedro@users.noreply.github.com>
2024-03-06 03:24:41 +0000

    atualização de conteúdo

--- script.js
+++ script.js
@@ -4,3 +4,25 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+const darkModeBtn = document.getElementById('dark-mode-btn');
+const body = document.body;
+
+darkModeBtn.addEventListener('click', () => {
+  body.classList.toggle('dark-theme');
+});
+
+// Função para exibir o modal de preview
+  function showPreview(event) {
+    document.querySelector('.modal').classList.add('active');
+    var modal = document.getElementById("previewModal");
+    modal.style.display = "block";
+    // Posiciona o modal próximo ao mouse
+    modal.style.top = (event.clientY + 10) + "px";
+    modal.style.left = (event.clientX + 10) + "px";
+  }
+
+  // Função para esconder o modal de preview
+  function hidePreview() {
+    document.querySelector('.modal').classList.remove('active');
+  }
